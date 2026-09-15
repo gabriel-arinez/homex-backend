513cf5f67eabc8fe203d30c68a0e1cff988eceda
gabriel-arinez <gabrielarinez056@gmail.com>
2026-09-15 01:45:54 -0400

    config: Configurar CORS

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,6 +69,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    os.getenv(
+        "CORS_ALLOWED_ORIGIN",
+        "http://localhost:5173",
+    ),
 ]
 
 ROOT_URLCONF = 'config.urls'
